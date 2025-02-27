23314d8aac9166cfb74788fb209856ed91508060
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-02-27 13:22:48 -0500

    Add editing functionality to Education.jsx

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -9,6 +9,7 @@ function Education({ data, onSubmit }) {
     }))
   );
   const [editMode, setEditMode] = useState(false);
+  const [selectedEducationId, setSelectedEducationId] = useState(null);
 
   function handleChange(e, id) {
     const { name, value } = e.target;
@@ -23,82 +24,94 @@ function Education({ data, onSubmit }) {
     e.preventDefault();
     onSubmit(formData);
     setEditMode(false);
+    setSelectedEducationId(null);
   }
 
   function handleAdd() {
     const newEntry = {
       id: uuidv4(),
-      schoolName: "",
+      schoolName: "New Education",
       degree: "",
       startDate: "",
       endDate: "",
       location: "",
     };
     setFormData([...formData, newEntry]);
+  }
+
+  function handleEdit(id) {
+    setSelectedEducationId(id);
     setEditMode(true);
   }
 
   return (
     <div className="education-info-form form-info">
-      <h2>Education</h2>
+      <h2> <i className="fa fa-graduation-cap"></i> Education</h2>
       {editMode ? (
         <form onSubmit={handleSubmit}>
-          {formData.map((item) => (
-            <div key={item.id}>
-                <label for="schoolName">School Name: </label>
+          {formData
+            .filter((item) => item.id === selectedEducationId)
+            .map((item) => (
+              <div key={item.id}>
+                <label htmlFor="schoolName">School Name: </label>
                 <input
-                    type="text"
-                    name="schoolName"
-                    value={item.schoolName}
-                    onChange={(e) => handleChange(e, item.id)}
-                    placeholder="School Name"
-                    required
+                  type="text"
+                  name="schoolName"
+                  value={item.schoolName}
+                  onChange={(e) => handleChange(e, item.id)}
+                  placeholder="School Name"
+                  required
                 />
-                <label for="degree">Degree: </label>
+                <label htmlFor="degree">Degree: </label>
                 <input
-                    type="text"
-                    name="degree"
-                    value={item.degree}
-                    onChange={(e) => handleChange(e, item.id)}
-                    placeholder="Degree"
-                    required
+                  type="text"
+                  name="degree"
+                  value={item.degree}
+                  onChange={(e) => handleChange(e, item.id)}
+                  placeholder="Degree"
+                  required
                 />
-                <label for="startDate">Start Date: </label>
+                <label htmlFor="startDate">Start Date: </label>
                 <input
-                    type="text"
-                    name="startDate"
-                    value={item.startDate}
-                    onChange={(e) => handleChange(e, item.id)}
-                    required
+                  type="text"
+                  name="startDate"
+                  value={item.startDate}
+                  onChange={(e) => handleChange(e, item.id)}
+                  required
                 />
-                <label for="endDate">End Date: </label>
+                <label htmlFor="endDate">End Date: </label>
                 <input
-                    type="text"
-                    name="endDate"
-                    value={item.endDate}
-                    onChange={(e) => handleChange(e, item.id)}
-                    required
+                  type="text"
+                  name="endDate"
+                  value={item.endDate}
+                  onChange={(e) => handleChange(e, item.id)}
+                  required
                 />
-                <label for="location">Location: </label>
+                <label htmlFor="location">Location: </label>
                 <input
-                    type="text"
-                    name="location"
-                    value={item.location}
-                    onChange={(e) => handleChange(e, item.id)}
-                    placeholder="Location"
-                    required
+                  type="text"
+                  name="location"
+                  value={item.location}
+                  onChange={(e) => handleChange(e, item.id)}
+                  placeholder="Location"
+                  required
                 />
-                </div>
+              </div>
             ))}
-            <button type="submit">Submit</button>
-            <button onClick={handleAdd}>+ Education</button>
-
+          <button type="submit">Save</button>
         </form>
       ) : (
-        <div>
-          <button onClick={() => setEditMode(true)}>Edit Education Details</button>
-        </div>
+        <>       
+         {formData.map((item) => (
+          <button key={item.id} onClick={() => handleEdit(item.id)}>
+            {item.schoolName}
+          </button>
+        ))}
+        {/* <button onClick={() => setEditMode(true)}>Edit Education Details</button> */}
+        </>
       )}
+      
+      <button onClick={handleAdd}>+ Education</button>
     </div>
   );
 }
