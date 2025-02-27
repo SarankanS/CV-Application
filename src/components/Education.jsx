7b2d365c9c70bfae887926684fef0a5c74ab8648
Sarankan Srikaran <sarankansrikaran@gmail.com>
2025-02-27 10:28:13 -0500

    Add labels to forms and add edit education button

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -39,69 +39,66 @@ function Education({ data, onSubmit }) {
   }
 
   return (
-    <div className="education-info form-info">
+    <div className="education-info-form form-info">
       <h2>Education</h2>
       {editMode ? (
         <form onSubmit={handleSubmit}>
           {formData.map((item) => (
             <div key={item.id}>
-              <input
-                type="text"
-                name="schoolName"
-                value={item.schoolName}
-                onChange={(e) => handleChange(e, item.id)}
-                placeholder="School Name"
-                required
-              />
-              <input
-                type="text"
-                name="degree"
-                value={item.degree}
-                onChange={(e) => handleChange(e, item.id)}
-                placeholder="Degree"
-                required
-              />
-              <input
-                type="date"
-                name="startDate"
-                value={item.startDate}
-                onChange={(e) => handleChange(e, item.id)}
-                required
-              />
-              <input
-                type="date"
-                name="endDate"
-                value={item.endDate}
-                onChange={(e) => handleChange(e, item.id)}
-                required
-              />
-              <input
-                type="text"
-                name="location"
-                value={item.location}
-                onChange={(e) => handleChange(e, item.id)}
-                placeholder="Location"
-                required
-              />
-            </div>
-          ))}
-          <button type="submit">Submit</button>
+                <label for="schoolName">School Name: </label>
+                <input
+                    type="text"
+                    name="schoolName"
+                    value={item.schoolName}
+                    onChange={(e) => handleChange(e, item.id)}
+                    placeholder="School Name"
+                    required
+                />
+                <label for="degree">Degree: </label>
+                <input
+                    type="text"
+                    name="degree"
+                    value={item.degree}
+                    onChange={(e) => handleChange(e, item.id)}
+                    placeholder="Degree"
+                    required
+                />
+                <label for="startDate">Start Date: </label>
+                <input
+                    type="text"
+                    name="startDate"
+                    value={item.startDate}
+                    onChange={(e) => handleChange(e, item.id)}
+                    required
+                />
+                <label for="endDate">End Date: </label>
+                <input
+                    type="text"
+                    name="endDate"
+                    value={item.endDate}
+                    onChange={(e) => handleChange(e, item.id)}
+                    required
+                />
+                <label for="location">Location: </label>
+                <input
+                    type="text"
+                    name="location"
+                    value={item.location}
+                    onChange={(e) => handleChange(e, item.id)}
+                    placeholder="Location"
+                    required
+                />
+                </div>
+            ))}
+            <button type="submit">Submit</button>
+            <button onClick={handleAdd}>+ Education</button>
+
         </form>
       ) : (
         <div>
-          {formData.map((data) => (
-            <div key={data.id}>
-              <p>School: {data.schoolName}</p>
-              <p>Degree: {data.degree}</p>
-              <p>Start Date: {data.startDate}</p>
-              <p>End Date: {data.endDate}</p>
-              <p>Location: {data.location}</p>
-            </div>
-          ))}
           <button onClick={() => setEditMode(true)}>Edit Education Details</button>
         </div>
       )}
-      <button onClick={handleAdd}>+ Education</button>
     </div>
   );
 }
